45609e10f9c9ee9c1f8e0c402794a3ac84d3e848
Akash Roshan <akash.second1999@gmail.com>
2020-05-27 23:45:13 +0530

    add more commands

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix = '.')
+bot = commands.Bot(command_prefix = '')
 
 # runs when the bot is running
 @bot.event
@@ -19,5 +19,13 @@ async def on_member_remove(member):
 @bot.command()
 async def ping(context):
     await context.send('Pong!')
+
+@bot.command()
+async def ding(context):
+    await context.send('Dong!')
+
+@bot.command()
+async def king(context):
+    await context.send('Kong!')
 
 bot.run('NzEyNTY3MjEzMjIzMDUxMzM1.XsTcWg.1BKJ23Xq0us_FPLPQUVnrfjpOK4')
